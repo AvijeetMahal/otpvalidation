d75dd4bde5ed7551aaafd1e3564f176180dc2435
Avijeet Mahal <mahalavijeet5@gmail.com>
2025-05-17 15:15:37 +0530

    last commit

--- server/server.js
+++ server/server.js
@@ -7,12 +7,13 @@ import authRouter from './routes/authRoutes.js';
 import userRouter from './routes/userRoutes.js';
 
 const app = express();
-
+const cors=require('cors');
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins=['http://localhost:5173']
+// const allowedOrigins=['http://localhost:5173']
 app.use(express.json()); 
+app.use(cors())
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials:true}))
 
