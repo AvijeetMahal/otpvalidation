5a877ceb2bb3f3a283afb7f5eb76d4fd3272b9f2
Avijeet Mahal <mahalavijeet5@gmail.com>
2025-05-18 15:36:08 +0530

    New

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,28 +1,22 @@
 import jwt from "jsonwebtoken";
 
-
 const userAuth = async (req, res, next) => {
-
     const { token } = req.cookies;
     if (!token) {
-        return res.json({ success: false, message: "Not authorized Please login again" })
+        return res.status(401).json({ success: false, message: "Not authorized. Please login again." });
     }
 
     try {
         const tokenDecode = jwt.verify(token, process.env.JWT_SECRET);
-        if (tokenDecode.id) {
+        if (tokenDecode && tokenDecode.id) {
             req.body.userId = tokenDecode.id;
+            next();
         } else {
-            return res.json({ success: false, message: "Not authorized Please login again" })
+            return res.status(401).json({ success: false, message: "Not authorized. Please login again." });
         }
-        next();
-
     } catch (error) {
-        res.json({ success: false, message: "Something went wrong" });
-
+        res.status(401).json({ success: false, message: "Invalid or expired token." });
     }
-
-
 };
 
 export default userAuth;
